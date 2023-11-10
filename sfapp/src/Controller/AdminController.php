b2a84fa35c8230ebe39610a57f18ce3971ca944f
mdesch01 <maxime.deschamps@etudiant.univ-lr.fr>
2023-11-10 01:34:42 +0100

    <feat>: finish popup addRoom, and almost include this popup in dashboard

--- sfapp/src/Controller/AdminController.php
+++ sfapp/src/Controller/AdminController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Room;
 use App\Form\AddRoomFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,24 +14,44 @@ use Symfony\Component\HttpFoundation\Request;
 class AdminController extends AbstractController
 {
     #[Route('/', name: 'app_admin')]
-    public function index(?int $id, ManagerRegistry $doctrine, ManagerRegistry $room): Response
+    public function index(?int $id, ManagerRegistry $doctrine, ManagerRegistry $room, Request $request): Response
     {
         $entityManager = $doctrine->getManager();
         $repository = $entityManager->getRepository('App\Entity\Room');
-        $room = $repository->findAll();
+        $rooms = $repository->findAll();
+        $room = new Room();
+
+        $form = $this->createForm(AddRoomFormType::class, $room);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($room);
+            $entityManager->flush();
+
+        }
+
         return $this->render('admin/index.html.twig', [
             'controller_name' => 'IndexController',
-            'listRooms' => $room,
+            'listRooms' => $rooms,
             'id' => $id,
+            'addRoomForm' => $form
+
         ]);
     }
 
     #[Route('/addRoom', name: 'addRoom')]
-    public function new(Request $request): Response
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $room = new Room();
 
         $form = $this->createForm(AddRoomFormType::class, $room);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($room);
+            $entityManager->flush();
+
+        }
 
         return $this->render('partial/popUpAddRoomForm.html.twig', [
             'addRoomForm' => $form
