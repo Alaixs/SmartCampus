1a2e6d1c4d79f45985d41af768c922fadfadaf40
mdesch01 <maxime.deschamps@etudiant.univ-lr.fr>
2023-11-14 15:14:47 +0100

    <feat>: add asterisk for all required element in popup

--- sfapp/src/Form/AddRoomFormType.php
+++ sfapp/src/Form/AddRoomFormType.php
@@ -3,33 +3,41 @@
 namespace App\Form;
 
 use App\Entity\Room;
+use \App\Form\MyFormFormatType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Choice;
 
-class AddRoomFormType extends AbstractType
+class AddRoomFormType extends MyFormFormatType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('name', null, [
-                'label' => 'Nom de la salle*'
+                'required' => true,
+                'label' => 'Nom de la salle ',
             ])
+
             ->add('floor', null, [
-                'label' => 'Etage*'
+                'required' => true,
+                'label' => 'Etage'
             ])
             ->add('capacity', null, [
-                'label' => 'Capacité maximale*'
+                'required' => true,
+                'label' => 'Capacité maximale'
             ])
             ->add('hasComputers', null, [
+                'required' => true,
                 'label' => 'Comporte des ordinateurs'
             ])
             ->add('area', null, [
-                'label' => 'Surface*'
+                'required' => true,
+                'label' => 'Surface'
             ])
             ->add('exposure', ChoiceType::class, [
+                'required' => true,
                 'choices' => [
                     'Nord' => 'north',
                     'Sud' => 'south',
@@ -37,16 +45,18 @@ class AddRoomFormType extends AbstractType
                     'Ouest' => 'west'
                 ],
 
-                'label' => 'Exposition*'
+                'label' => 'Exposition'
             ])
             ->add('nbWindows', null, [
-                'label' => 'Nombre de fenêtres*'
-            ])
-        ;
+                'required' => true,
+                'label' => 'Nombre de fenêtres'
+            ]);
+
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
+
         $resolver->setDefaults([
             'data_class' => Room::class,
         ]);
