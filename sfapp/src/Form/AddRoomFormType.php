c990699bce59cba5abb822cfd55f645c7db7783e
mdesch01 <maxime.deschamps@etudiant.univ-lr.fr>
2023-11-14 15:16:46 +0100

    <feat>: fix error for the popup form

--- sfapp/src/Form/AddRoomFormType.php
+++ sfapp/src/Form/AddRoomFormType.php
@@ -3,14 +3,13 @@
 namespace App\Form;
 
 use App\Entity\Room;
-use \App\Form\MyFormFormatType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Choice;
 
-class AddRoomFormType extends MyFormFormatType
+class AddRoomFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
