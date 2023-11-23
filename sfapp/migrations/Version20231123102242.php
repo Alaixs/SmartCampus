7e1e2d4aac42c5238090a9ae64777dd4ec253d50
mtaraud <maxime.taraud@etudiant.univ-lr.fr>
2023-11-23 11:23:36 +0100

    fix: fix of the script startPbBD

--- sfapp/migrations/Version20231123102242.php
+++ sfapp/migrations/Version20231123102242.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20231123094824 extends AbstractMigration
+final class Version20231123102242 extends AbstractMigration
 {
     public function getDescription(): string
     {
--- startPbBD.sh
+++ startPbBD.sh
@@ -4,5 +4,5 @@
 docker compose up --build -d
 
 # Execute commands inside the docker container
-docker compose exec sfapp bash -c 'cd sfapp/ && php bin/console doctrine:database:drop --force && rm -rf migrations/* && php bin/console doctrine:database:create && /app/sfapp$ php bin/console make:migration && composer install && php bin/console doctrine:migrations:migrate << echo ``'
+docker compose exec sfapp bash -c 'cd sfapp/ && php bin/console doctrine:database:drop --force && rm -rf migrations/* && php bin/console doctrine:database:create && php bin/console make:migration && composer install && php bin/console doctrine:migrations:migrate << echo ``'
 
