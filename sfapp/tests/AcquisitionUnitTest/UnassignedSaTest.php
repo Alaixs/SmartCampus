4312702e3cc3c5838d224a728ac41d035ff097ff
mdesch01 <maxime.deschamps@etudiant.univ-lr.fr>
2023-12-20 11:56:00 +0100

    test: add tests

---
+++ sfapp/tests/AcquisitionUnitTest/UnassignedSaTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Tests;
+
+use App\Domain\StateSA;
+use App\Entity\Room;
+use App\Repository\AcquisitionUnitRepository;
+use App\Entity\AcquisitionUnit;
+use App\Repository\RoomRepository;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class UnassignSaTest extends WebTestCase
+{
+
+}
