a7b69105fe52e98d67a6c7988073ecf86f08ecfb
mdesch01 <maxime.deschamps@etudiant.univ-lr.fr>
2024-01-10 17:07:40 +0100

    test: test get data api

---
+++ sfapp/tests/GetDataTest/GetDataApiTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Tests;
+
+use App\Entity\AcquisitionUnit;
+use App\Entity\Room;
+use App\Infrastructure\GetDataAPI;
+use Monolog\Test\TestCase;
+
+class GetDataApiTest extends TestCase {
+
+    public function testValidGetLastDataByType()
+    {
+
+        $getData = new GetDataAPI();
+
+        // Create Room and AU
+        $au = new AcquisitionUnit();
+        $au->setName('ESP-009');
+        $room = new Room();
+        $room->setAcquisitionUnit($au);
+
+
+        $value = $getData->getLastValueByType($room, 'hum');
+
+        $this->assertTrue($value[0] != -1, 'WOw');
+    }
+
+    public function testInvalidGetLastDataByType()
+    {
+        $getData = new GetDataAPI();
+
+        // Create Room and AU
+        $au = new AcquisitionUnit();
+        $au->setName('ESP-845');
+        $room = new Room();
+        $room->setAcquisitionUnit($au);
+
+
+        $value = $getData->getLastValueByType($room, 'hum');
+
+        $this->assertTrue($value[0] == -1 && $value[1] == 0, 'WOw');
+    }
+}
