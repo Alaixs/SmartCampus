8ed983c2a4308a9ce1d383710f89a4bcec5a4eb9
tberr <theo.berr@etudiant.univ-lr.fr>
2024-01-12 15:06:34 +0100

    tests: access to installation manual done

--- sfapp/tests/TechnicienTest/AccessToInstallationManualTest.php
+++ sfapp/tests/TechnicienTest/AccessToInstallationManualTest.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class AccessToInstallationManualTest extends WebTestCase
 {
-    public function testFooterLinkIsPresentForTechnician()
+    public function testTechnicianHasAccessToInstallationManual()
     {
         $client = static::createClient();
 
@@ -19,6 +19,33 @@ class AccessToInstallationManualTest extends WebTestCase
         $link = $crawler->filter('footer .installation-manual a:contains("Télécharger le manuel d\'installation du SA")')->count();
         $this->assertGreaterThan(0, $link, 'Le lien <a> dans le footer est présent à l\'écran pour le technicien.');
     }
+
+    public function testMissionHeadHasNotAccessToInstallationManual()
+    {
+        $client = static::createClient();
+
+        $this->login($client, 'yacine', 'jesuisyacine');
+
+        $crawler = $client->getCrawler();
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+
+        $link = $crawler->filter('footer .installation-manual a:contains("Télécharger le manuel d\'installation du SA")')->count();
+        $this->assertEquals(0, $link, 'Le lien <a> dans le footer n\'est présent à l\'écran pour le chargé de mission.');
+    }
+
+    public function testNotLoggedUserHasNotAccessToInstallationManual()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+
+        $link = $crawler->filter('footer .installation-manual a:contains("Télécharger le manuel d\'installation du SA")')->count();
+        $this->assertEquals(0, $link, 'Le lien <a> dans le footer ne devrait pas être présent à l\'écran sans connexion.');
+    }
+
 
     private function login($client, $username, $password)
     {
