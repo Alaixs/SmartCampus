6a022d0c813547aaa8e1d4f972c1449bedfc954a
mdesch01 <maxime.deschamps@etudiant.univ-lr.fr>
2024-01-14 01:01:44 +0100

    feat: add comfort level of room data on dashboard of yacine

--- sfapp/src/Controller/AdminController.php
+++ sfapp/src/Controller/AdminController.php
@@ -12,18 +12,21 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class AdminController extends AbstractController
 {
 
     #[Route('/admin', name: 'app_admin')]
-    public function admin(RoomRepository $roomRepository, AcquisitionUnitRepository $acquisitionUnitRepository, EntityManagerInterface $entityManager, GetDataInteface $getData,?AcquisitionUnit $acquisitionUnit = null): Response {
+    public function admin(UserInterface $user, RoomRepository $roomRepository, AcquisitionUnitRepository $acquisitionUnitRepository,
+                          EntityManagerInterface $entityManager, GetDataInteface $getData,?AcquisitionUnit $acquisitionUnit = null): Response {
         $roomList = $roomRepository->findAll();
         $acquisitionUnitList = $acquisitionUnitRepository->findAll();
+        $roomsComfortIndicator = null;
 
-        $roomsComfortIndicator = $getData->getRoomsComfortIndicator($roomList);
-
+        if (in_array('ROLE_ADMIN', $user->getRoles(), true)) {
+            $roomsComfortIndicator = $getData->getRoomsComfortIndicator($roomList);
+        }
 
 
 
