e768223b49cca3df052245334895b7a2aa7b41a3
tberr <theo.berr@etudiant.univ-lr.fr>
2024-01-15 16:37:35 +0100

    docs: add of comments on test file

--- sfapp/tests/TechnicienTest/AccessToInstallationManualTest.php
+++ sfapp/tests/TechnicienTest/AccessToInstallationManualTest.php
@@ -6,6 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class AccessToInstallationManualTest extends WebTestCase
 {
+    /**
+     * La méthode testTechnicianHasAccessToInstallationManual() vérifie si une fois connecté,
+     * le technicien peut voir le texte "Consulter le manuel d'installation du SA".
+     * @return void
+     */
     public function testTechnicianHasAccessToInstallationManual()
     {
         $client = static::createClient();
@@ -16,10 +21,15 @@ class AccessToInstallationManualTest extends WebTestCase
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
 
-        $link = $crawler->filter('footer .installation-manual a:contains("Télécharger le manuel d\'installation du SA")')->count();
+        $link = $crawler->filter('footer .installation-manual a:contains("Consulter le manuel d\'installation du SA")')->count();
         $this->assertGreaterThan(0, $link, 'Le lien <a> dans le footer est présent à l\'écran pour le technicien.');
     }
 
+    /**
+     * La méthode testMissionHeadHasNotAccessToInstallationManual() vérifie si une fois connecté,
+     * le chargé de mission ne peut pas voir pas le texte "Consulter le manuel d'installation du SA".
+     * @return void
+     */
     public function testMissionHeadHasNotAccessToInstallationManual()
     {
         $client = static::createClient();
@@ -30,10 +40,15 @@ class AccessToInstallationManualTest extends WebTestCase
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
 
-        $link = $crawler->filter('footer .installation-manual a:contains("Télécharger le manuel d\'installation du SA")')->count();
+        $link = $crawler->filter('footer .installation-manual a:contains("Consulter le manuel d\'installation du SA")')->count();
         $this->assertEquals(0, $link, 'Le lien <a> dans le footer n\'est présent à l\'écran pour le chargé de mission.');
     }
 
+    /**
+     * La méthode testNotLoggedUserHasNotAccessToInstallationManual() vérifie si un usager lambda qui
+     * ne se connecte pas, ne peut pas voir le texte "Consulter le manuel d'installation du SA".
+     * @return void
+     */
     public function testNotLoggedUserHasNotAccessToInstallationManual()
     {
         $client = static::createClient();
@@ -46,7 +61,14 @@ class AccessToInstallationManualTest extends WebTestCase
         $this->assertEquals(0, $link, 'Le lien <a> dans le footer ne devrait pas être présent à l\'écran sans connexion.');
     }
 
-    private function login($client, $username, $password)
+
+    /**
+     * La méthode login() connecte un client avec son identifiant et son mot de passe
+     * @param $client
+     * @param string $username
+     * @param string $password
+     */
+    private function login($client, string $username, string $password): void
     {
         $crawler = $client->request('GET', '/login');
 
